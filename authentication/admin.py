6e979947f8f4bf4865e56e5af85a60404a73248b
Dimitar Peev <dpeev95@gmail.com>
2020-02-17 16:39:22 +0200

    upgrade admin panels

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "bio", "first_name", "last_name", "date_joined", "last_active", )
+    search_fields = ("email", "bio", "first_name", "last_name", )
--- programs/admin.py
+++ programs/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Program, ProgramAdMembership
 
 
-# Register your models here.
-admin.site.register(Program)
-admin.site.register(ProgramAdMembership)
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("creator", "title", "description", "created_at", "updated_at")
+    search_fields = ("=creator__id", "title", "description", )
+
+
+@admin.register(ProgramAdMembership)
+class ProgramAdMembershipAdmin(admin.ModelAdmin):
+    list_display = ("ad", "program", "ad_index", "created_at", "updated_at")
+    search_fields = ("=ad__id", "=program__id")
